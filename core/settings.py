41a533f76b461b8197914552515b22bb2047df5e
Guilherme M. Miranda <alchemist.software@proton.me>
2022-12-26 23:29:25 -0300

    Trimming

--- core/settings.py
+++ core/settings.py
@@ -4,9 +4,8 @@ from os import environ
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 ALLOWED_HOSTS = environ.get('ALLOWED_HOSTS', ['localhost', '127.0.0.1', '::1'])
-SECRET_KEY = environ.get('SECRET_KEY', )
+SECRET_KEY = environ.get('SECRET_KEY')
 DEBUG = environ.get('DEBUG', 1)
-
 
 INSTALLED_APPS = [
 	"django.contrib.admin",
@@ -15,6 +14,9 @@ INSTALLED_APPS = [
 	"django.contrib.sessions",
 	"django.contrib.messages",
 	"django.contrib.staticfiles",
+
+	"rest_framework",
+	"rest_framework_simplejwt",
 ]
 
 MIDDLEWARE = [
@@ -26,6 +28,17 @@ MIDDLEWARE = [
 	"django.contrib.messages.middleware.MessageMiddleware",
 	"django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+# REST_FRAMEWORK = {
+# 	'DEFAULT_PERMISSION_CLASSES': [
+# 		'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+# 		'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+# 	],
+# 	'DEFAULT_AUTHENTICATION_CLASSES': (
+# 		'rest_framework.authentication.SessionAuthentication',
+# 		'rest_framework_simplejwt.authentication.JWTAuthentication',
+# 	)
+# }
 
 ROOT_URLCONF = "core.urls"
 
@@ -47,10 +60,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "core.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
 	"default": {
 		"ENGINE": "django.db.backends.sqlite3",
@@ -58,38 +67,17 @@ DATABASES = {
 	}
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
-	{
-		"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-	},
+	{"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",},
 	{"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
 	{"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
 	{"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.1/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
-
 TIME_ZONE = "UTC"
-
 USE_I18N = True
-
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = "static/"
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
